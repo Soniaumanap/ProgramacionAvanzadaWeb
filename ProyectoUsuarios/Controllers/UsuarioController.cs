0041f5a1a8bf9f1c87da59b53e00750fe63ef465
rangulo <rangulo@CR9-ITS-RANGULO>
2025-10-04 18:09:28 -0600

    Primer commit del proyecto ProyectoUsuarios

---
+++ ProyectoUsuarios/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ProyectoUsuarios.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly ILogger<UsuarioController> logger;
+        private readonly IUsuariosServicio _usuariosServicio;
+        public UsuarioController(ILogger<UsuarioController> logger, IUsuariosServicio usuariosServicio)
+        {
+            this.logger = logger;
+            _usuariosServicio = usuariosServicio;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var respuesta = await _usuariosServicio.ObtenerUsuarioAsync();
+            return View(respuesta.Data);
+        }
+    }
+}
