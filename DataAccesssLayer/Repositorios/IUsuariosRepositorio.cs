b2d1673eb9d4e92b485fe426191fb231afb062f7
rangulo <rangulo@CR9-ITS-RANGULO>
2025-10-07 17:47:37 -0600

    Agrega capas BusinessLogicLayer y DataAccesssLayer y actualiza solución

---
+++ DataAccesssLayer/Repositorios/IUsuariosRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using DataAccesssLayer.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesssLayer.Repositorios
+{
+    public interface IUsuariosRepositorio
+    {
+        Task<List<Usuario>> ObtenerUsuarioAsync();
+        Task<Usuario> ObtenerUsuarioPorIdAsync(int id);
+        Task<bool> AgregarUsuarioAsync(Usuario usuario);
+        Task<bool> ActualizarUsuarioAsync(Usuario usuario);
+        Task<bool> EliminarUsuarioAsync(int id);
+    }
+}
