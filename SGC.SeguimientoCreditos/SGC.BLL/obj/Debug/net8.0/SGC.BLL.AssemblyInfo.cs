da67b577280f8d26bdd88320c66b777d48422340
Gustavo Marin <wstov@hotmail.com>
2025-12-14 11:17:52 -0600

    Ajustes de API
    Ajustes de API

--- SGC.SeguimientoCreditos/SGC.BLL/obj/Debug/net8.0/SGC.BLL.AssemblyInfo.cs
+++ SGC.SeguimientoCreditos/SGC.BLL/obj/Debug/net8.0/SGC.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGC.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bac4ebbbdd4bff163635a8aa7c182589afcca64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0a9196adfff237080c78e376aa55c6611844979")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGC.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGC.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
